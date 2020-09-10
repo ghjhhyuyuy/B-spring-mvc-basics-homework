2a68d94a73ab9e898fc6da711b03d1c0619cf8c9
zuowen.Wang <798389920@qq.com>
2020-09-10 13:07:29 +0800

    add test should_throw_exception_when_password_is_invalid

--- src/test/java/com/thoughtworks/capacity/gtb/mvc/service/UserOperationServiceTest.java
+++ src/test/java/com/thoughtworks/capacity/gtb/mvc/service/UserOperationServiceTest.java
@@ -22,23 +22,23 @@ public class UserOperationServiceTest {
     private final List<User> userList = new ArrayList<>();
     @Before
     public void setUp() {
-        User user = new User(1,"作文","798@qq.com","123");
+        User user = new User(1,"作文","798@qq.com","123123");
         userList.add(user);
     }
     @Test
     public void should_add_user_when_given_correct_parameters(){
         UserOperationService userOperationService = new UserOperationService(userList);
-        User user = new User(null,"zuowen",null,"111");
+        User user = new User(null,"zuowen",null,"111111");
         userOperationService.register(user);
         assertEquals(2,userList.size());
         assertEquals("zuowen",userList.get(1).getUserName());
-        assertEquals("111",userList.get(1).getPassword());
+        assertEquals("111111",userList.get(1).getPassword());
     }
 
     @Test
     public void should_throw_exception_when_username_is_null(){
         UserOperationService userOperationService = new UserOperationService(userList);
-        User user = new User(null,null,null,"111");
+        User user = new User(null,null,null,"111111");
         Throwable exception = assertThrows(Exception.class,
                 () -> userOperationService.register(user));
         assertEquals("用户名不能为空",exception.getMessage());
@@ -56,9 +56,18 @@ public class UserOperationServiceTest {
     @Test
     public void should_throw_exception_when_username_is_invalid(){
         UserOperationService userOperationService = new UserOperationService(userList);
-        User user = new User(null,"zuo-wen",null,null);
+        User user = new User(null,"zuo-wen",null,"123123");
         Throwable exception = assertThrows(Exception.class,
                 () -> userOperationService.register(user));
         assertEquals("用户名不合法",exception.getMessage());
+    }
+
+    @Test
+    public void should_throw_exception_when_password_is_invalid(){
+        UserOperationService userOperationService = new UserOperationService(userList);
+        User user = new User(null,"zuowen",null,"123");
+        Throwable exception = assertThrows(Exception.class,
+                () -> userOperationService.register(user));
+        assertEquals("密码不合法",exception.getMessage());
     }
 }
